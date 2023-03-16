4ca4c3df0c90c4d6363b976f66946a99d88d6e5e
ber <yanqinglou@outlook.com>
2023-03-15 21:17:35 -0700

    add personal introduction

--- src/components/pages/aboutme.js
+++ src/components/pages/aboutme.js
@@ -13,6 +13,9 @@ const styles = {
     display: "flex",
     alignItems: "center",
   },
+  aboutmep: {
+  padding:'20px'
+  },
   
 };
 
@@ -23,18 +26,14 @@ export default function Aboutme() {
       <br></br>
       <div style={styles.aboutme}>
         <img style={styles.aboutmeImg} src={img}></img>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-          velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia
-          nisi ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex.
-          Cras non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna
-          imperdiet ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum
-          tortor. Cras rutrum ligula in tincidunt commodo. Morbi sit amet mollis
-          orci, in tristique ex. Donec nec ornare elit. Donec blandit est sed
-          risus feugiat porttitor. Vestibulum molestie hendrerit massa non
-          consequat. Vestibulum vitae lorem tortor. In elementum ultricies
-          tempus. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-        </p>
+        <div>
+        <p style={styles.aboutmep}>
+          My name is Yanqing Lou. I'm currently pursuing a cerficate from University of Washington to become a full stack web developer. 3 moths ago, I knew almost nothing about programming and web design. Throughout this program, I have learnt how to write code in Javascript, how to structre a web page in html, how to style a page in css, how to construct data structure in mysql and mongo and how to build a responsive webpage through React. I believe what I have learnt throughout this course will be strong evidence of my learning ability and my problem-solving skills. Most importly, I'm now able to solve issues in both work and life in a computer-thinking way.
+        </p>       
+         <p style={styles.aboutmep}>Career-wise, I'm inspired to become a product specialist/manager, managint the daily activities of developing/developed product.
+          </p>
+        </div>
+  
       </div>
     </div>
   );
